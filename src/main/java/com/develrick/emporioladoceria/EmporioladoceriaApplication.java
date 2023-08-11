fb427fad93159ed4fc07225131e5d9c33d070394
Elrick Fernando <elrickfernando60@gmail.com>
2023-08-11 00:28:17 -0300

    Criação do Projeto Emporio La Doceria

---
+++ src/main/java/com/develrick/emporioladoceria/EmporioladoceriaApplication.java
@@ -0,0 +1,13 @@
+package com.develrick.emporioladoceria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmporioladoceriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmporioladoceriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develrick/emporioladoceria/EmporioladoceriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develrick.emporioladoceria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmporioladoceriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
